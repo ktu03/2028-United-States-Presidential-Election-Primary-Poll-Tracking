531b0e0f9d4367c8165ca9ef5a3fa8897585cc8a
ktu03 <ktu3666@gmail.com>
2025-08-29 22:52:43 -0700

    Poll

---
+++ McLaughlin & Associates 2025-06-10.sql
@@ -0,0 +1,24 @@
+## McLaughlin & Associates 2025-06-10
+
+USE `2028 United States Presidential Election`;
+
+INSERT INTO `Republican_Party_Primary_Polling` (Candidate, Polling, Pollster, Nationwide, Sample_Size, Voter_Type, DMY, Link)
+Values ('J.D. Vance',36, 'McLaughlin & Associates',TRUE	,455, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Nikki Haley',4, 'McLaughlin & Associates',TRUE	,455, 'Likely Voters','2025-06-10','hhttps://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Vivek Ramaswamy',2, 'McLaughlin & Associates',TRUE	,455, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Marco Rubio',4, 'McLaughlin & Associates',TRUE	,455, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Ron Desantis',6, 'McLaughlin & Associates',TRUE	,455, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf');
+
+INSERT INTO `Democratic_Party_Primary_Polling` (Candidate, Polling, Pollster, Nationwide, Sample_Size, Voter_Type, DMY, Link)
+Values ('Kamala Harris',30, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Cory Booker',7, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Pete Buttigieg',10, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Gavin Newsom',8, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Alexandria Ocasio-Cortez',7, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Josh Shapiro',3, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Tim Walz',4, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Amy Klobuchar',2, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Gretchen Whitmer',3, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('J.B. Pritzker',1, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Rahm Emanuel',2, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf'),
+('Roy Cooper',1, 'McLaughlin & Associates',TRUE	,434, 'Likely Voters','2025-06-10','https://mclaughlinonline.com/pols/wp-content/uploads/2025/06/National-June-Presentation-RELEASE-6-16-25.pdf');
